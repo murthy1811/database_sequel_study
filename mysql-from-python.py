8f3a59b2e6ba6ce17b19f0501d3e910132b0f5f7
Murthy Valavala <murthy.mailme@gmail.com>
2021-08-16 04:26:30 +0000

    python to mysql study

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,9 +12,9 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    with connection.cursor() as cursor:        
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                          Friends(name char(20), age int, DOB datetime);""")
+    with connection.cursor() as cursor:          
+        cursor.execute("UPDATE Friends SET age = %s WHERE name = %s;", (23, 'Bob'))
+        connection.commit()
         #Note that the above will still display a warning (not error) if the
         # table already exists
 finally:
